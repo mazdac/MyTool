7dd1d98508198494f07c6924111740de18fcd450
water_fairy <995637517@qq.com>
2017-04-13 13:36:35 +0800

    添加库

---
+++ app/src/main/java/com/waterfairy/tool/jni/MyJni.java
@@ -0,0 +1,13 @@
+package com.waterfairy.tool.jni;
+
+/**
+ * Created by water_fairy on 2017/4/7.
+ */
+
+public class MyJni {
+//    {
+//        System.loadLibrary("JniTest");
+//
+//    }
+//    public static native String sayHello();
+}
